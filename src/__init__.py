aa2f79f30a452c80925c5dd16b43036583ff8da7
MarcosTG1 <marcostudela04@gmail.com>
2025-12-01 20:00:42 +0100

    chore: añadir dependencia xlrd y actualizar exports del paquete src
    - Añadir 'xlrd' a requirements.txt para soporte de archivos .xls.
    - Exponer funciones de servicios y calendario en src/__init__.py para facilitar importaciones.

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,26 @@
 # Este archivo permite que Python trate el directorio 'src' como un paquete.
 __version__ = "0.1.0"
+
+# Define qué se exporta cuando se hace: from src import *
+
+from .services.ManejoDeEventos import (
+    AnadirEventosCalendarioJustificantes,
+    AnadirEventosCalendarioIncidencias,
+    AnadirEventosCalendarioTrafico
+)
+
+from .services.ManejoGoogleCalendar import (
+    get_calendar_service,
+    add_event,
+    delete_event
+)
+
+# Definir qué se exporta con from src import *
+__all__ = [
+    'AnadirEventosCalendarioJustificantes',
+    'AnadirEventosCalendarioIncidencias',
+    'AnadirEventosCalendarioTrafico',
+    'get_calendar_service',
+    'add_event',
+    'delete_event',
+]
